2d96caa3825443d444fd83ca3df48e7352e31be1
kluzko <kluzniakkuba@gmail.com>
2025-02-06 22:13:23 +0100

    test: parser unit tests

--- src/parser.rs
+++ src/parser.rs
@@ -27,3 +27,38 @@ impl DukascopyParser {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_tick() {
+        let data = vec![
+            0x00, 0x00, 0x00, 0x01, // ms = 1
+            0x00, 0x00, 0x00, 0x02, // ask = 2
+            0x00, 0x00, 0x00, 0x03, // bid = 3
+            0x40, 0x00, 0x00, 0x00, // ask_volume = 2.0
+            0x40, 0x80, 0x00, 0x00, // bid_volume = 4.0
+        ];
+
+        let result = DukascopyParser::parse_tick(&data).unwrap();
+        assert_eq!(result, (1, 0.00002, 0.00003, 2.0, 4.0));
+    }
+
+    #[test]
+    fn test_parse_tick_invalid_data() {
+        let data = vec![0x00; 19]; // Incomplete data
+        let result = DukascopyParser::parse_tick(&data);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_validate_decompressed_data() {
+        let valid_data = vec![0x00; 20]; // Valid data (multiple of 20)
+        let invalid_data = vec![0x00; 19]; // Invalid data (not multiple of 20)
+
+        assert!(DukascopyParser::validate_decompressed_data(&valid_data).is_ok());
+        assert!(DukascopyParser::validate_decompressed_data(&invalid_data).is_err());
+    }
+}
